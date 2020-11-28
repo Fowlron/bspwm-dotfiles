8fac8b2809dc3fe4743c0d0958db76c442c7be0a
Fowlron <pedromfgodinho@gmail.com>
2020-11-28 04:01:25 +0000

    Solved errors in install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-xinit lightdm noto-fonts noto-fonts-emoji lightdm-slick-greeter bspwm sxhkd zsh pulseaudio pulseaudio-alsa pavucontrol kitty dmenu xbindkeys light feh picom polkit mate-polkit neovim xclip python python2 python-pynvim nodejs npm firefox tree unzip p7zip neofetch wget lolcat fortune-mod cowsay vlc
+sudo pacman -S xorg xorg-xinit lightdm noto-fonts noto-fonts-emoji bspwm sxhkd zsh pulseaudio pulseaudio-alsa pavucontrol kitty dmenu xbindkeys light feh picom polkit mate-polkit neovim xclip python python2 python-pynvim nodejs npm firefox tree unzip p7zip neofetch wget lolcat fortune-mod cowsay vlc
 
-yay -S lightdm-slick-greeter lf-bin bottom-bin python2-nvim
+yay -S lightdm-slick-greeter lf-bin bottom-bin python2-pynvim
 
 # keeping this line to remind myself to do it
 # sudo localectl set-x11-keymap pt
@@ -14,8 +14,8 @@ sudo systemctl enable lightdm.service
 # https://www.reddit.com/r/nordtheme/comments/hxldpu/nordic_obsession/
 sudo wget https://i.imgur.com/shyNwsK.png -O /usr/share/pixmaps/wallpaper.jpg
 
-sudo echo "[Greeter]" > /etc/lightdm/slick-greeter.conf
-sudo echo "background=/usr/share/pixmaps/wallpaper.jpg" >> /etc/lightdm/slick-greeter.conf
+sudo sh -c 'echo "[Greeter]" > /etc/lightdm/slick-greeter.conf'
+sudo sh -c 'echo "background=/usr/share/pixmaps/wallpaper.jpg" >> /etc/lightdm/slick-greeter.conf'
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -36,9 +36,9 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 echo ".dotfiles" > .gitignore
 git clone --bare https://github.com/Fowlron/bspwm-dotfiles.git $HOME/.dotfiles
-dotfiles stash
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME stash
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
 # TODO polybar
 # TODO kitty keybinds
